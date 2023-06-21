bce07532a1a52015a896736591da8f04d71b972c
João Victor <joaovictorvieira@outlook.com.br>
2023-06-21 16:53:12 -0400

    21-06-23 atualização 3
    planos

--- planos.php
+++ planos.php
@@ -91,8 +91,7 @@ $urlBase = "http://localhost/apartamentoavendacuiaba/real-estate/";
     <link rel="icon" type="image/x-icon" href="imagem/icon.png">
     <!-- metas -->
     <meta property="og:title" content="Apartamento a Venda Cuiaba" />
-    <meta property="og:description"
-        content="Encontre os diversos apartamentos em todos os bairros de Cuiabá que estãõ anunciados a venda." />
+    <meta property="og:description" content="Encontre os diversos apartamentos em todos os bairros de Cuiabá que estãõ anunciados a venda." />
     <meta property="og:url" content="https://www.apartamentoavendacuiaba.com.br" />
     <meta property="og:image" content="" />
 
@@ -103,8 +102,7 @@ $urlBase = "http://localhost/apartamentoavendacuiaba/real-estate/";
     <meta property="og:image:alt" content="" />
     <!-- CSS only -->
     <link rel="stylesheet" href="css/style.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
     <title>Planos | Apartamento a Venda Cuiabá</title>
 </head>
 
@@ -127,15 +125,15 @@ $urlBase = "http://localhost/apartamentoavendacuiaba/real-estate/";
                 <div class="planoMainTitulo col-md-8 mx-auto text-center">
                     <h1 class="mb-5">Anuncie seu apartamento em Cuiabá e alcance os compradores certos!</h1>
                     <a href="#" class="text-center">
-                        <div class="imgPlan alert alert-warning col-md-3 mx-auto text-center" role="alert">
-                            <img src="imagem/plan.svg" width="40"> <span> &nbsp; Planos</span>
+                        <div class="imgPlan col-md-4 mx-auto text-center" role="alert">
+                            <img src="imagem/plan.svg" width="40"> <span> &nbsp; Planos Premium</span>
                         </div>
                     </a>
 
                 </div>
             </div>
             <div class="row text-center">
-                <div class="col-md-4 adsMainBox">
+                <div class="col-md-4 adsMainBox my-2">
                     <div class="adsMainBoxContent">
                         <div class="adsMainBoxContentIcon">
                             <img src="imagem/apartamento.svg" width="40" alt="" srcset="">
@@ -148,7 +146,7 @@ $urlBase = "http://localhost/apartamentoavendacuiaba/real-estate/";
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 adsMainBox">
+                <div class="col-md-4 adsMainBox my-2">
                     <div class="adsMainBoxContent">
                         <div class="adsMainBoxContentIcon">
 
@@ -163,7 +161,7 @@ $urlBase = "http://localhost/apartamentoavendacuiaba/real-estate/";
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 adsMainBox">
+                <div class="col-md-4 adsMainBox my-2">
                     <div class="adsMainBoxContent">
                         <div class="adsMainBoxContentIcon">
 
@@ -188,21 +186,61 @@ $urlBase = "http://localhost/apartamentoavendacuiaba/real-estate/";
                     <h1 class="mb-5">Qual plano PREMUIM se adequa melhor para você?</h1>
                 </section>
 
-                <div class="col-md-8 mx-auto">
-                    <div class="col-md-6 text-center">
-                        <div class="planoCard">
-                            <div class="planoCardHeader">
-                                Plano Premium Mensal
-                            </div>
-                            <div class="planoCardBody">
-                                <h4>Anúncios Ilimitados</h4>
-                                <p>R$ <span class="planoPrice">97,90</span> <span class="planoTempo"> / mês</span> </p>
-                                <a href="#" class="planoCardBotao">Contratar Agora</a>
-                            </div>
-                            <div class="planoCardRodape">
-                                <p>Insira anúncios <strong>ilimitados</strong>  por mês</p>
+                <div class="col-md-8 mx-auto precoCore">
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+
+                            <div class="planoCard">
+                                <div class="planoCardHeader">
+                                    Plano Premium Mês
+                                </div>
+                                <div class="planoCardBody">
+                                    <h4>Anúncios Ilimitados</h4>
+                                    <p>R$ <span class="planoPrice">97,90</span> <span class="planoTempo"> / mês</span> </p>
+                                    <a href="#" class="planoCardBotao">Contratar Agora</a>
+                                </div>
+                                <div class="planoCardRodape">
+                                    <p>Insira anúncios <strong>ilimitados</strong> por mês</p>
+                                </div>
                             </div>
                         </div>
+                        <div class="col-md-6  text-center">
+                            <div class="planoCard">
+                                <div class="planoCardHeader">
+                                    <span class="desconto">-50%OFF</span> Plano Premium <span class="anual">Ano</span>
+                                </div>
+                                <div class="planoCardBody">
+                                    <h4>Anúncios Ilimitados</h4>
+                                    <p>R$ <span class="planoPrice">497,90</span> <span class="planoTempo"> / ano</span> </p>
+                                    <a href="#" class="planoCardBotao">Contratar Agora</a>
+                                </div>
+                                <div class="planoCardRodape">
+                                    <p>Insira anúncios <strong>ilimitados</strong> por ano</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mx-auto formas-pgto text-center mt-5">Formas de pagamento aceitas:</div>
+                    <div class="col-md-6 mx-auto formas-pgto text-center"><img src="imagem/pgto.png" alt=""></div>
+                    <div class="col-md-6 mx-auto formas-pgto text-center">O plano tem cobrança recorrente, até o cancelamento. Você pode cancelar a qualquer momento sem multa.</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="social">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto text-center">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h1>titulo</h1>
+                            <h2>subtitulo</h2>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, ut illo doloremque dignissimos expedita repudiandae? Et a alias animi? Distinctio sint consequuntur facilis vero exercitationem error? Natus temporibus consequatur saepe, unde quam provident minima fuga ea, corporis iusto veritatis porro!</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem quisquam ab nihil modi vero enim culpa, atque eaque ipsam facilis voluptate excepturi exercitationem saepe? Consectetur ducimus ipsa laudantium sed eos!</p>
+                            <p class="chamada">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, officiis.</p>
+                        </div>
+                        <div class="col-md-6">Imagem</div>
                     </div>
                 </div>
             </div>
@@ -220,9 +258,7 @@ $urlBase = "http://localhost/apartamentoavendacuiaba/real-estate/";
     </footer>
 
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
 
 </body>
